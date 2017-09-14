b5193ca9b9067482dbd39592cc43498f0f8f8f86
lsteil <lsteil@inf.ufsm.br>
2017-09-14 16:31:04 -0300

    Refatoração Models 1

--- backend/API_REST/serializers.py
+++ backend/API_REST/serializers.py
@@ -1,36 +1,49 @@
-from API_REST.models import User
+from API_REST.models import Student, Course
 
 from django.contrib.auth import update_session_auth_hash
 
 from rest_framework import serializers
 
 
-class UserSerializer(serializers.ModelSerializer):
-
+class StudentSerializer(serializers.ModelSerializer):
+    confirm_password= serializers.CharField(write_only=True)
     
     class Meta:
-        model = User
-        fields = ('username', 'first_name', 'last_name', 'email', 'password')
+        model = Student
+        fields = ('fullName', 'registration', 'id_course', 'email', 'rg', 'cpf', 'phone1', 'password', 'confirm_password')
 
 
 
     def create(self, validated_data):
-        user = User(
+        student = Student(
             email=validated_data['email'],
-            username=validated_data['username']
+            registration=validated_data['registration'],
+            phone1=validated_data['phone1'],
+            rg=validated_data['rg'],
+            cpf=validated_data['cpf'],
+            fullName=validated_data['fullName']
+
         )
-        user.set_password(validated_data['password'])
-        user.save()
-        return user
+        student.set_password(validated_data['password'])
+        student.save()
+        return student
 
     def validate(self, data):
         '''
         Ensure the passwords are the same
         '''
+        print(data)
         if data['password']:
-            print "Here"
+            print ("Here")
             if data['password'] != data['confirm_password']:
                 raise serializers.ValidationError(
                     "The passwords have to be the same"
                 )
         return data
+
+
+class CourseSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Course
+        fields = ('courseName', 'duration', '_type')
--- backend/API_REST/urls.py
+++ backend/API_REST/urls.py
@@ -1,6 +1,6 @@
 from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token, verify_jwt_token
 
-from API_REST.views import UserViewSet, UserRegister
+from API_REST.views import StudentViewSet, StudentRegister, CourseViewSet
 from API_REST import views
 
 from django.conf.urls import url, include
@@ -10,27 +10,31 @@ from rest_framework import renderers
 
 
 
-user_list = UserViewSet.as_view({
+student_list = StudentViewSet.as_view({
     'get': 'list',
+    'get': 'retrive'
 })
-'''user_detail = UserViewSet.as_view({d
-    'get': 'retrieve',
-    'post': 'create'
-})
-user_create = UserViewSet.as_view({
-    'post': 'create'
-})'''
 
-user_register = UserRegister.as_view({
-    'post': 'create'
+student_register = StudentRegister.as_view({
+    'post': 'create',
+    'get': 'list'
+})
+
+course_list = CourseViewSet.as_view({
+    'post': 'create',
+    'get': 'list'
 })
 
 urlpatterns = [
     url(r'^login', obtain_jwt_token),
-    url(r'^users', user_list, name='user-list'),
-    url(r'^register', UserViewSet),
     url(r'^token-refresh/', refresh_jwt_token),
     url(r'^token-verify/', verify_jwt_token),
+
+    url(r'^users', student_list, name='user-list'),
+    url(r'^register', student_register, name='student-register'),
+
+    url(r'^course', course_list, name='course-list'),
+    
     # url(r'^users/new/$', user_create),
     # url(r'^users/(?P<pk>[0-9]+)/$', user_detail, name='user-detail'),
 
